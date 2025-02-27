e26630f4a5b1179748a221db0531ea2ca5100e9f
nguyệt thúy <144653260+NguyenThuy02@users.noreply.github.com>
2025-02-27 14:23:50 +0700

    update UI

--- src/components/learning/Pagetwo.js
+++ src/components/learning/Pagetwo.js
@@ -3,17 +3,40 @@ import React from "react";
 
 const Pagetwo = () => {
   return (
-    <>
-      <div>
-        <table>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Description</th>
-          </tr>
-        </table>
-      </div>
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th>Họ đệm</th>
+          <th>Tên</th>
+          <th>Năm sinh</th>
+          <th>Địa chỉ</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Nguyet</td>
+          <td>Thuy</td>
+          <td>0</td>
+          <td>Quy Nhơn</td>
+          <td>ex@gmail.com</td>
+        </tr>
+        <tr>
+          <td>Nguyet</td>
+          <td>Thuy</td>
+          <td>0</td>
+          <td>Quy Nhơn</td>
+          <td>ex@gmail.com</td>
+        </tr>
+        <tr>
+          <td>Nguyet</td>
+          <td>Thuy</td>
+          <td>0</td>
+          <td>Quy Nhơn</td>
+          <td>ex@gmail.com</td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
